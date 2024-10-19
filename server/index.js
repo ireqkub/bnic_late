d7df15b2e4d35c786dfac91a7b790d0a1053ff3e
ireqkub <0953452988win@gmail.com>
2024-10-19 16:45:15 +0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -2,8 +2,13 @@ const express = require("express");
 const app = express()
 const port = 5000
 
+
 app.use("/", (req, res) => {
     res.send("Server is running")
 })
 
-app.listen(port, console.log("Server started on port: "+port))
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
+
+
